1f572c8491d290785ceddf39ee0c650443a0cd48
Sushant Thakur <69715019+SushanThakur@users.noreply.github.com>
2021-07-11 09:27:17 +0545

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    const preloder = document.querySelector('.preloder');
+    preloder.classList.add('invisible');
+})
---
+++ js/nav.js
@@ -0,0 +1,34 @@
+const open = document.getElementById('open')
+    close = document.getElementById('close')
+    navbarmobile = document.getElementById('mobilenav')
+    text1 = document.getElementById('text1')
+    text2 = document.getElementById('text2')
+    bg = document.getElementById('bgSection')
+    nav = document.querySelector('nav');
+
+open.addEventListener('click', () => {
+    nav.classList.add('active');
+    open.classList.add('invisible');
+    close.classList.add('visible');
+    navbarmobile.classList.add('clicked');
+    text1.classList.add('invisible');
+    text2.classList.add('visible');
+})
+
+close.addEventListener('click', () => {
+    nav.classList.remove('active');
+    open.classList.remove('invisible');
+    close.classList.remove('visible');
+    navbarmobile.classList.remove('clicked');
+    text1.classList.remove('invisible');
+    text2.classList.remove('visible');
+})
+
+bg.addEventListener('click', () => {
+    nav.classList.remove('active');
+    open.classList.remove('invisible');
+    close.classList.remove('visible');
+    navbarmobile.classList.remove('clicked');
+    text1.classList.remove('invisible');
+    text2.classList.remove('visible');    
+})
